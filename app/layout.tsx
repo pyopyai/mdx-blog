481fe15e46d49646fc7d2db6913efa68a2e769d2
pyopyai <mahuuzinn@gmail.com>
2025-11-14 18:30:25 +0900

    ModeToggle追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider"
+import {ModeToggle} from "@/components/ui/ModeToggle"
 
 import "./globals.css";
 
@@ -34,6 +35,7 @@ export default function RootLayout({
           disableTransitionOnChange
           > 
           {children}
+          <ModeToggle />
         </ThemeProvider>
       </body>
     </html>
